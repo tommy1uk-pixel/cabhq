d3fbdd58dbee7e680c23714f3ae118e1b2a1b863
tommy1uk-pixel <tommy1uk@hotmail.co.uk>
2026-04-17 23:46:05 +0100

    update root route

--- cabhq-backend/src/app.controller.ts
+++ cabhq-backend/src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  health() {
+    return {
+      status: 'ok',
+      app: 'CABHQ Backend',
+      message: 'Backend is live',
+    };
   }
 }
