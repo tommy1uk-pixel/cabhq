acf217ad1951782a88f57676341f203d951b76a1
tommy1uk-pixel <tommy1uk@hotmail.co.uk>
2026-05-15 10:22:03 +0100

    Fix cors properly

--- cabhq-backend/src/main.ts
+++ cabhq-backend/src/main.ts
@@ -5,10 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      process.env.FRONTEND_URL,
-    ],
+    origin: true,
     credentials: true,
   });
 
