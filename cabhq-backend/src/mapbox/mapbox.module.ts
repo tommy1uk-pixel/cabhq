950cdb25fdeedb5a71ccceeb275607279da04643
tommy1uk-pixel <tommy1uk@hotmail.co.uk>
2026-05-16 18:02:50 +0100

    Fix mapbox locations dependency

--- cabhq-backend/src/mapbox/mapbox.module.ts
+++ cabhq-backend/src/mapbox/mapbox.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { LocationsModule } from '../locations/locations.module';
 import { MapboxController } from './mapbox.controller';
 import { MapboxService } from './mapbox.service';
 
 @Module({
+  imports: [LocationsModule],
   controllers: [MapboxController],
   providers: [MapboxService],
-  exports: [MapboxService],
 })
 export class MapboxModule {}
